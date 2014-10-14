fbe45d56c74941be284e0e173695163854dfb03e
jkymmel <jkymmel@gmail.com>
2014-10-14 08:43:14 +0300

    Commented config file

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
-const DB_HOST = "localhost";
-const DB_DB = "askaquestion";
-const DB_USER = "root";
-const DB_PW = "";
-const SPAMTIMER = 0;
-const DUPLICATTIMER = 300;
-const ADMIN_PW = "Leenu1337";
+const DB_HOST = "localhost"; // MySQL server address
+const DB_DB = "askaquestion"; // MySQL database name
+const DB_USER = "root"; // MySQL username
+const DB_PW = ""; // MySQL password
+const SPAMTIMER = 5; // Time in seconds between which users can make post. DISABLE (SET TO 0) IF MANY USE THE SAME IP!!!
+const DUPLICATTIMER = 300; // Time in seconds between which user can reenter already entered message from same IP
+const ADMIN_PW = "Leenu1337"; // Admin PW, keep secret!
 ?>
